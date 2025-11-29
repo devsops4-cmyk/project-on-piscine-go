857da12079cb92c176dc23b741b38d56079c3cfc
devsops4-cmyk <devsops4@gmail.com>
2025-11-29 11:27:54 +0100

    Add files via upload

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,26 +1,22 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	var words []string
-	wordStart := -1
+	word := ""
+	var result []string
 
-	for i, char := range s {
-		if char != ' ' && char != '\t' && char != '\n' {
-			if wordStart == -1 {
-				wordStart = i // start of a word
+	for _, t := range s {
+		if t == ' ' || t == '\t' || t == '\n' {
+			if word != "" {
+				result = append(result, word)
+				word = ""
 			}
 		} else {
-			if wordStart != -1 {
-				words = append(words, s[wordStart:i]) // end of a word
-				wordStart = -1
-			}
+			word += string(t)
 		}
 	}
-
-	// add the last word if the string didn't end with a separator
-	if wordStart != -1 {
-		words = append(words, s[wordStart:])
+	if word != "" {
+		result = append(result, word)
 	}
 
-	return words
+	return result
 }
